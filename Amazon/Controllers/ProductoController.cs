9e52b8f4d4e3c64e8615cc644a9acd034864ab13
FranciscoIxta <francisco.cota21@uabc.edu.mx>
2023-10-03 23:57:51 -0700

    Update

---
+++ Amazon/Controllers/ProductoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Amazon.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public ActionResult Detalle(id)
+        //{
+        //    var producto = ObtenerDetalleDelProducto(id);
+
+        //    return View(producto);
+        //}
+    }
+}
